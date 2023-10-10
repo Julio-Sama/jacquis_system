40ee9a2e5220d1421a3aada93c7aa779f44f2756
ollantay <luis.vaquerano07@gmail.com>
2023-10-10 11:26:48 -0600

    PDF Empleados

--- src/main/java/com/jacquis/jacquis_system/controllers/PDFExportController.java
+++ src/main/java/com/jacquis/jacquis_system/controllers/PDFExportController.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
 
+import org.hibernate.annotations.GeneratorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
@@ -12,15 +13,24 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.jacquis.jacquis_system.model.Empleado;
 import com.jacquis.jacquis_system.model.Proveedor;
+import com.jacquis.jacquis_system.repository.EmpleadoRepository;
 import com.jacquis.jacquis_system.repository.ProveedorRepository;
 import com.jacquis.jacquis_system.services.DatabasePDFService;
+import com.jacquis.jacquis_system.services.EmpleadoPDFService;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class PDFExportController {
 
     @Autowired
     ProveedorRepository proveedorRepository;
+
+    @Autowired
+    EmpleadoRepository empleadoRepository;
 
     @GetMapping(value = "/openpdf/proveedores", produces = MediaType.APPLICATION_PDF_VALUE)
     public ResponseEntity<InputStreamResource> proveedorReport() throws IOException {
@@ -34,4 +44,18 @@ public class PDFExportController {
         return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF)
                 .body(new InputStreamResource(bis));
     }
+
+    @GetMapping(value="/openpdf/empleados", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<InputStreamResource> empleadoReport() throws IOException  {
+        List<Empleado> empleado = (List<Empleado>) empleadoRepository.findAll();
+
+        ByteArrayInputStream bis = EmpleadoPDFService.empleadoPDFReport(empleado);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Disposition", "inline; filename=reporte_empleados.pdf");
+
+        return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(bis));
+    }
+    
 }
---
+++ src/main/java/com/jacquis/jacquis_system/services/EmpleadoPDFService.java
@@ -0,0 +1,102 @@
+package com.jacquis.jacquis_system.services;
+
+import java.awt.Color;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.apache.pdfbox.multipdf.PDFCloneUtility;
+import org.apache.poi.common.usermodel.fonts.FontFamily;
+
+import com.jacquis.jacquis_system.model.Empleado;
+import com.jacquis.jacquis_system.model.Proveedor;
+import com.lowagie.text.Chunk;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
+
+public class EmpleadoPDFService {
+ 
+    public static ByteArrayInputStream empleadoPDFReport(List<Empleado> empleado) {
+        Document document = new Document();
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+ 
+        try {   
+            PdfWriter.getInstance(document, out);   
+            document.open();
+ 
+            // Add Content to PDF file ->
+            Font fontHeader = FontFactory.getFont(FontFactory.defaultEncoding, 15);
+            Paragraph para = new Paragraph("Informe de empleados - Jacquis Store", fontHeader);
+            para.setAlignment(Element.ALIGN_CENTER);
+            document.add(para);
+            document.add(Chunk.NEWLINE);
+ 
+            PdfPTable table = new PdfPTable(6);
+            // Add PDF Table Header ->
+            Stream.of("ID","Nombres", "Apellidos", "Direccion", "Correo", "Estado Actual").forEach(headerTitle -> {
+                PdfPCell header = new PdfPCell();
+                Font headFont = FontFactory.getFont(FontFactory.defaultEncoding, 12);
+                header.setBackgroundColor(Color.PINK);
+                header.setHorizontalAlignment(Element.ALIGN_CENTER);
+                header.setBorderWidth(2);
+                header.setPhrase(new Phrase(headerTitle, headFont));
+                table.addCell(header);
+            });
+ 
+            for (Empleado emp : empleado) {  
+                PdfPCell id = new PdfPCell(new Phrase(emp.getId_empleado().toString()));
+                id.setPaddingLeft(1);
+                id.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                id.setHorizontalAlignment(Element.ALIGN_CENTER);
+                table.addCell(id);
+ 
+                PdfPCell nombrePDF = new PdfPCell(new Phrase(emp.getNombre()));
+                nombrePDF.setPaddingLeft(4);
+                nombrePDF.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                nombrePDF.setHorizontalAlignment(Element.ALIGN_CENTER);
+                table.addCell(nombrePDF);
+
+                PdfPCell apellidoPDF = new PdfPCell(new Phrase(emp.getApellido()));
+                apellidoPDF.setPaddingRight(4);
+                apellidoPDF.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                apellidoPDF.setHorizontalAlignment(Element.ALIGN_CENTER);
+                table.addCell(apellidoPDF);
+ 
+                PdfPCell direccionPDF = new PdfPCell(new Phrase(String.valueOf(emp.getDireccion())));
+                direccionPDF.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                direccionPDF.setHorizontalAlignment(Element.ALIGN_CENTER);
+                direccionPDF.setPaddingRight(4);
+                table.addCell(direccionPDF);
+ 
+                PdfPCell correoPDF = new PdfPCell(new Phrase(String.valueOf(emp.getCorreo_empleado())));
+                correoPDF.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                correoPDF.setHorizontalAlignment(Element.ALIGN_CENTER);
+                correoPDF.setPaddingRight(4);
+                table.addCell(correoPDF);
+ 
+                PdfPCell estadoPDF = new PdfPCell(new Phrase(String.valueOf(emp.getEstado_empleado())));
+                estadoPDF.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                estadoPDF.setHorizontalAlignment(Element.ALIGN_CENTER);
+                estadoPDF.setPaddingRight(4);
+                table.addCell(estadoPDF);
+
+            }
+            document.add(table);
+            
+            document.close();
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        }
+ 
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+}
