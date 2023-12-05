698c8a63aa33912349d0b79ea38e5216f43f0af6
ollantay <luis.vaquerano07@gmail.com>
2023-12-05 08:43:13 -0600

    Generador de codigo de barras - Producto

--- src/main/java/com/jacquis/jacquis_system/services/InventarioService.java
+++ src/main/java/com/jacquis/jacquis_system/services/InventarioService.java
@@ -5,8 +5,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
 import com.jacquis.jacquis_system.model.Inventario;
 import com.jacquis.jacquis_system.repository.InventarioRepository;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Service
 public class InventarioService {
@@ -24,6 +31,7 @@ public class InventarioService {
 
     public void saveOrUpdate(Inventario inventario) {
         inventarioRepository.save(inventario);
+        generateBarcode(inventario.getCodigo_producto());
     }
 
     public boolean existsById(Long id) {
@@ -32,5 +40,15 @@ public class InventarioService {
 
     public void delete(Long id) {
         inventarioRepository.deleteById(id);
+    }
+
+    public void generateBarcode(String codigo_producto){
+        try {
+            BitMatrix bitMatrix = new Code128Writer().encode(codigo_producto, BarcodeFormat.CODE_128, 300, 150);
+            Path path = Paths.get("C:\\xampp\\htdocs\\Barcodes\\"+codigo_producto+".png");
+            MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
